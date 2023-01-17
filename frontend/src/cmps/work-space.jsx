aa9ebd5897e559066fbc0b15e8c6e24d260f1b11
Amir Sela <amirsela10@gmail.com>
2023-01-17 21:42:20 +0200

    first layout

---
+++ frontend/src/cmps/work-space.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+export function WorkSpace() {
+
+    useState
+
+    return <section className="work-space">
+        im work space
+    </section>
+}
---
+++ frontend/src/pages/app-index.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+// import { loadBoards, addBoard, updateBoard, removeBoard, addToBoardt } from '../store/board.actions.js'
+
+// import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service.js'
+import { SideNav } from '../cmps/side-nav.jsx'
+import { WorkSpace } from '../cmps/work-space.jsx'
+import { BoardDetails } from '../cmps/board-details.jsx'
+import { Navigate } from 'react-router-dom'
+
+export function AppIndex() {
+
+    // const boards = useSelector(storeState => storeState.boardModule.boards)
+
+
+
+    function onOpenBoard(boardId) {
+        // setIsBoardOpen(!isMenuOpen)
+        Navigate(`/board/${boardId}`)
+    }
+
+
+
+    return <section className="app-index">
+
+        <SideNav />
+        <WorkSpace />
+        <BoardDetails />
+    </section>
+
+}
