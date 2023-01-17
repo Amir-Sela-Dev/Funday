28861e4a242919b26fc13388df1232bde22e7815
Amir Yakubov <yakobub@gmail.com>
2023-01-17 23:35:27 +0200

    add task list

---
+++ frontend/src/cmps/task/task-list.jsx
@@ -0,0 +1,17 @@
+
+export function TaskList({ group }) {
+
+    return (
+        <div className="task-list">
+            <div className="task-title-row flex">
+                <div>item</div>
+                <div>status</div>
+            </div>
+            {group.tasks.map(task =>
+                <div className="task-preview flex" key={task._id}>
+                    <div>{task.txt}</div>
+                    <div>  <button className="option-btn" onClick={() => { }}>...</button></div>
+                </div>)}
+        </div>
+    )
+}
