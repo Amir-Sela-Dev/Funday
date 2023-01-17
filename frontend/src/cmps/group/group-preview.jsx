e44c1b2923decee153173ab91a9c836b110b5439
Amir Sela <amirsela10@gmail.com>
2023-01-17 23:48:57 +0200

    end first day

--- frontend/src/cmps/group/group-preview.jsx
+++ frontend/src/cmps/group/group-preview.jsx
@@ -1,8 +1,12 @@
+import { TaskList } from "../task/task-list";
 
 export function GroupPreview({ group }) {
 
     return (
-        <div className="group-title"> {group.title} {group.tasks.length}</div>
+        <section className="group-preview">
+            <div className="group-title"> {group.title} {group.tasks.length} </div>
+            <TaskList group={group} />
+        </section>
     )
 
 
