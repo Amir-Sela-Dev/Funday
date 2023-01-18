6bfbe4aabca3ef70c4a27ad454ef9c56ca8d4bef
Amir Yakubov <yakobub@gmail.com>
2023-01-18 13:06:17 +0200

    add group style

--- frontend/src/cmps/task/task-list.jsx
+++ frontend/src/cmps/task/task-list.jsx
@@ -4,10 +4,18 @@ export function TaskList({ group }) {
 
     return (
         <div className="task-list">
+
             <div className="task-title-row flex">
-                <div>item</div>
-                <div>status</div>
+                <div className="checkbox-column task-column">
+                    <input className='task-checkbox' type="checkbox" />
+                </div>
+
+                <div className="task-title task-column">Item</div>
+                <div className="task-persons task-column"><span>Person</span></div>
+                <div className="task-status task-column">Status</div>
+                <div className="task-date task-column">Date</div>
             </div>
+
             {group.tasks.map(currTask => {
                 return <TaskPreview task={currTask} />
             })}
