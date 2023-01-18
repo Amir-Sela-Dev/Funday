e3a50df33c32b791741246fda472bb5d860d32db
Amir Yakubov <yakobub@gmail.com>
2023-01-18 03:43:06 +0200

    add styling

--- frontend/src/cmps/board/board-list.jsx
+++ frontend/src/cmps/board/board-list.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom"
 
 export function BoardList({ boards }) {
-
+    const boardIcon = 'board.svg'
+    const optionIcon = 'option-icon.svg'
     return (
         <ul className="boards-list">
             {boards.map(board =>
                 <li className="board-preview flex" key={board._id}>
-                    <div>
+                    <div className="board-wrap">
+                        <img className="board-icon" src={require(`/src/assets/img/${boardIcon}`)} />
                         <Link className="board-preview-link" to={`/board/${board._id}`}>{board.title}</Link>
-                        <button className="option-btn" onClick={() => { }}>...</button>
                     </div>
+                    <img className="option-icon board-icon" src={require(`/src/assets/img/${optionIcon}`)} />
                 </li>)}
         </ul>
     )
 }
+
--- frontend/src/cmps/side-nav.jsx
+++ frontend/src/cmps/side-nav.jsx
@@ -1,10 +1,13 @@
 
 
 export function SideNav() {
-
+    const logoUrl = 'logo.png'
+    const workManagmentUrl = 'work-managment.svg'
     return (
         <nav className="side-nav">
-            im nav
+            <img className="main-logo" src={require(`../assets/img/${logoUrl}`)} />
+            <div className="logo-hr"></div>
+            <img className="work-managment-icon" src={require(`../assets/img/${workManagmentUrl}`)} />
         </nav>
     )
 }
