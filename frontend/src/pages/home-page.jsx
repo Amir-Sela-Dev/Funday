8a0380af58598e2380047294570085cb50e7e984
Amir Sela <amirsela10@gmail.com>
2023-01-19 10:21:44 +0200

    home page done

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from "react-router-dom"
 // import logo from '../assets/img/logo.png'
 import { CHANGE_COUNT } from '../store/user.reducer'
 
@@ -10,8 +11,12 @@ export function HomePage() {
 
     return (
         <section className='home-page'>
+            <div className="home-page-main flex">
+                <h1> A platform built for a new way of working</h1>
+                <p>What would you like to manage with Funnday?</p>
+                <Link className='see-demo' to="/board/uPD5q"> See Demo ⇨</Link>
+            </div>
             <img className="cover-img" src={require(`/src/assets/img/${cover}`)} />
-            <Link className='see-demo' to="/board/uPD5q"> See Demo ⇨</Link>
         </section >
     )
 }
