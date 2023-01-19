54dfe72258fcbca4f37e4f24d80c74e639964839
ksheilan <119981956+ksheilan@users.noreply.github.com>
2023-01-19 16:47:48 +0200

    Added basic input to persons modal

--- frontend/src/cmps/task/person-details.jsx
+++ frontend/src/cmps/task/person-details.jsx
@@ -2,11 +2,12 @@ import { useState } from "react"
 
 export function PersonDetails({ persons }) {
     return (
-        <div className="person-details flex justify-between">
-            <div className="person-names">
+        <div className="person-details flex column">
+            <div className="person-names flex justify-between">
                 {persons &&
                     persons.map(person => { return <span>{person.fullname}</span> })}
             </div>
+            <input placeholder="Invite someone.."></input>
         </div>
     )
 }
