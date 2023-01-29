f7e555bc9d56c53adb66c72564dbf33d123e72a8
Amir Yakubov <yakobub@gmail.com>
2023-01-29 12:04:35 +0200

    add more style to homepage

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -17,22 +17,70 @@ export function HomePage() {
                 <p>Manage your information in an easy, clear, fast and smart way</p>
                 <Link className='see-demo' to={`/board/63d5b54e7d97b7406caecd80`}>get started ⇨</Link>
             </div>
+            <div className="triangle-wrapper">
+                <div className="triangle"></div>
+            </div>
             <div className="about-us flex">
                 <h2 className="about-title">Our Crew</h2>
                 <div className="main-avatars flex">
 
                     <div className="about-avatar">
                         <img src='https://res.cloudinary.com/dp3tok7wg/image/upload/v1674462764/img3_zynodi.jpg' />
+                        <p>Amir Sela</p>
                     </div>
 
                     <div className="about-avatar">
                         <img src='https://res.cloudinary.com/dp3tok7wg/image/upload/v1674462891/image_1_w0fgmh.png' />
+                        <p>Sheilan Shamilov</p>
                     </div>
 
                     <div className="about-avatar">
                         <img src='https://res.cloudinary.com/dp3tok7wg/image/upload/v1674462524/profile_vyll5h.jpg' />
+                        <p>Amir Yakubov</p>
                     </div>
 
+                </div>
+            </div>
+            <hr className="home-page-hr" />
+            <div className="our-clients">
+                <h2 className="about-title">Our Clients</h2>
+                <p>Trusted by 178,000+ customers worldwide
+                </p>
+                <div className="clients-img flex">
+                    <div className="img-wrap">
+                        <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/genpact.png" />
+                    </div>
+                    <div className="img-wrap">
+                        <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/HoltCat.png" />
+                    </div>
+
+                    <div className="img-wrap">
+                        <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/canva.png" />
+                    </div>
+
+                    <div className="img-wrap">
+                        <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/coca_cola.png" />
+                    </div>
+
+                    <div className="img-wrap">
+                        <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/lionsgate.png" />
+                    </div>
+
+                    <div className="img-wrap">
+                        <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/hulu.png" />
+                    </div>
+
+                    <div className="img-wrap">
+                        <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/bd.png" />
+                    </div>
+
+                    <div className="img-wrap">
+                        <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/Electronic_Arts.png" />
+                    </div>
+
+                    <div className="img-wrap">
+                        <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/universal.png" />
+                    </div>
                 </div>
             </div>
 
