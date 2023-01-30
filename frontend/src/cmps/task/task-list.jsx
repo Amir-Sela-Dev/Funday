f040ec3438f9a1008cd1675811c5da5c103fc384
Amir Yakubov <yakobub@gmail.com>
2023-01-30 13:29:41 +0200

    add navegation and board list modal for mobile

--- frontend/src/cmps/task/task-list.jsx
+++ frontend/src/cmps/task/task-list.jsx
@@ -231,7 +231,7 @@ export function TaskList({ group, tasks, toggleModal, setNewTasks }) {
 
                 <div className="add-task-wrap flex">
                     <div className="sticky-grid flex">
-                        <div class="white-background"></div>
+                        <div className="white-background"></div>
                         <div className='colored-tag task-column last-tag' style={{ background: group.style?.color || '#FFF000', border: 'none' }} />
 
                         <div className="checkbox-column task-column disabled">
