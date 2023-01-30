b36121c854a1d7dee148bf3f66a8bea0b452d1b7
ksheilan <sheilan102@gmail.com>
2023-01-30 12:55:14 +0200

    task preview comment users

--- frontend/src/cmps/task/task-updates.jsx
+++ frontend/src/cmps/task/task-updates.jsx
@@ -40,6 +40,7 @@ export function TaskUpdates({ board, group, task = '', formatTime }) {
         if (!value) return
         comment.txt = value
         comment.createdAt = Date.now()
+        comment.byMember = user
         task.comments.unshift(comment)
 
         await saveTask(board, group.id, task)
@@ -73,9 +74,9 @@ export function TaskUpdates({ board, group, task = '', formatTime }) {
 
                     <div className="user-line flex justify-between">
                         <div className="flex align-center">
-                            <img src={(user?.imgUrl) ? user.imgUrl : 'https://res.cloudinary.com/dp3tok7wg/image/upload/v1674331758/g-profile_zylwbg.png'} alt="" className="user" />
+                            <img src={comment.byMember.imgUrl || 'https://res.cloudinary.com/dp3tok7wg/image/upload/v1674331758/g-profile_zylwbg.png'} alt="" className="user" />
                             {/* <img src='https://res.cloudinary.com/dp3tok7wg/image/upload/v1674331758/g-profile_zylwbg.png' alt="" className="user" /> */}
-                            <a> {user?.fullname} </a>
+                            <a> {comment.byMember.fullname} </a>
                             <div className="is-active"></div>
                         </div>
                         <div className="date flex align-center">
